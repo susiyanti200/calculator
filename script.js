58fc445f56dd6b35e25f8fe9b0c93d50de416809
susiyanti <susiyanti.g11@gmail.com>
2021-05-04 16:36:38 +0700

    Fix edge case
    1. Account for operator pressed one after another
    2. Account for pressing '=' directly after operator
    3. Accoutn for displaying long floating point or large number

--- script.js
+++ script.js
@@ -3,23 +3,36 @@ const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
 
 let acc = 0;
-let newNumber = false;
-let op;
+let isNewNumber = false;
+let op = null;
 
-const displayNumber = (e) => {
-  if (display.textContent.length > 15) return;
+const displayNumber = (num) => {
+  let numText = num.toString();
+  if (numText.length <= 15) {
+    display.textContent = num;
+  } else {
+    if (numText.includes(".")) {
+      display.textContent = parseFloat(num.toFixed(6));
+    } else {
+      display.textContent = num.toExponential(6);
+    }
+  }
+};
+
+const setNumber = (e) => {
+  if (!isNewNumber && display.textContent.length > 15) return;
   if (e.target.textContent === ".") {
-    if (display.textContent.includes(".")) return;
-    display.textContent = newNumber
+    if (!isNewNumber && display.textContent.includes(".")) return;
+    display.textContent = isNewNumber
       ? "0" + e.target.textContent
       : display.textContent + e.target.textContent;
   } else {
     display.textContent =
-      display.textContent === "0" || newNumber
+      display.textContent === "0" || isNewNumber
         ? e.target.textContent
         : display.textContent + e.target.textContent;
   }
-  newNumber = false;
+  isNewNumber = false;
 };
 
 const evaluate = (num1, num2) => {
@@ -38,41 +51,49 @@ const evaluate = (num1, num2) => {
 };
 
 const clear = () => {
-  acc = 0;
-  newNumber = true;
+  isNewNumber = true;
   op = null;
 };
 
 const doOperation = (e) => {
   switch (e.target.textContent) {
     case "C":
-      display.textContent = acc;
+      displayNumber(0);
       clear();
+      acc = 0;
       break;
     case "DEL":
-      display.textContent =
-        display.textContent.length > 1
-          ? display.textContent.slice(0, display.textContent.length - 1)
-          : 0;
+      display.textContent.length > 1
+        ? displayNumber(
+            display.textContent.slice(0, display.textContent.length - 1)
+          )
+        : displayNumber(0);
       break;
     case "+":
     case "-":
     case "*":
     case "/":
-      newNumber = true;
-      acc = evaluate(acc, parseFloat(display.textContent));
-      display.textContent = acc;
+      // if an operator is pressed after number then calculate the previous operation
+      // if an operator is pressed one after another then just update to the newest operator
+      if (!isNewNumber) {
+        acc = evaluate(acc, parseFloat(display.textContent));
+        displayNumber(acc);
+        isNewNumber = true;
+      }
       op = e.target.textContent;
       break;
-    default:
-      acc = evaluate(acc, parseFloat(display.textContent));
-      display.textContent = acc;
-      clear();
+    case "=":
+      // calculate operation if number is pressed after operator
+      if (!isNewNumber) {
+        acc = evaluate(acc, parseFloat(display.textContent));
+        displayNumber(acc);
+        clear();
+      }
   }
 };
 
 numbers.forEach((number) => {
-  number.addEventListener("click", displayNumber);
+  number.addEventListener("click", setNumber);
 });
 
 operations.forEach((op) => {
